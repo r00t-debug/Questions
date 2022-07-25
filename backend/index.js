92441ca244e6b55e0fbdd0fd56bc42bac9e98c85
imaginaries <hsissen0@gmail.com>
2022-07-25 11:52:49 +0100

    changes

--- backend/index.js
+++ backend/index.js
@@ -47,10 +47,10 @@ app.get('/api/pdf', (req, res) => {
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('frontend/build'))
+  app.use(express.static('../frontend/build'))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'))
   })
 }
 
